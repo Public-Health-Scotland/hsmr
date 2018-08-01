3aab397dd01de675c61530551d92a36b126e1c01
David Caldwell <david.caldwell1@nhs.net>
2018-08-01 14:42:23 +0100

    Separate SQL code for trends and hsmr

--- R/sql_queries.R
+++ R/sql_queries.R
@@ -53,14 +53,3 @@ z_query_gro <- paste("select LINK_NO, DATE_OF_DEATH",
                      "from ANALYSIS.GRO_DEATHS_C",
                      "where DATE_OF_DEATH >= to_date(",z_start_date,",'yyyy-MM-dd') AND DATE_OF_DEATH <= to_date('2017-09-30','yyyy-MM-dd')",
                      "ORDER BY LINK_NO")
-
-z_query_smr01_ltt <- paste("select LINK_NO, ADMISSION_DATE, DISCHARGE_DATE, CIS_MARKER, POSTCODE, SPECIALTY,",
-                           "ADMISSION_TYPE, DISCHARGE_TYPE, HBTREAT_CURRENTDATE,",
-                           "CASE WHEN age_in_years BETWEEN 0 AND 19 THEN '1'",
-                           "WHEN age_in_years BETWEEN 20 AND 39 THEN '2'",
-                           "WHEN age_in_years BETWEEN 40 AND 59 THEN '3'",
-                           "WHEN age_in_years BETWEEN 60 AND 79 THEN '4'",
-                           "WHEN age_in_years >= 80 THEN '5'",
-                           "ELSE 'NULL' END age_grp from SMR01_PI",
-                           "where ADMISSION_DATE >= to_date(",z_start_date,", 'yyyy-MM-dd') AND ADMISSION_DATE <= to_date(",z_end_date,",'yyyy-MM-dd')",
-                           "ORDER BY LINK_NO, ADMISSION_DATE, RECORD_TYPE, DISCHARGE_DATE, ADMISSION, DISCHARGE, URI")
---
+++ R/sql_queries_ltr.R
@@ -0,0 +1,10 @@
+z_query_smr01_ltt <- paste("select LINK_NO, ADMISSION_DATE, DISCHARGE_DATE, CIS_MARKER, POSTCODE, SPECIALTY,",
+                           "ADMISSION_TYPE, DISCHARGE_TYPE, HBTREAT_CURRENTDATE,",
+                           "CASE WHEN age_in_years BETWEEN 0 AND 19 THEN '1'",
+                           "WHEN age_in_years BETWEEN 20 AND 39 THEN '2'",
+                           "WHEN age_in_years BETWEEN 40 AND 59 THEN '3'",
+                           "WHEN age_in_years BETWEEN 60 AND 79 THEN '4'",
+                           "WHEN age_in_years >= 80 THEN '5'",
+                           "ELSE 'NULL' END age_grp from SMR01_PI",
+                           "where ADMISSION_DATE >= to_date(",z_start_date,", 'yyyy-MM-dd') AND ADMISSION_DATE <= to_date(",z_end_date,",'yyyy-MM-dd')",
+                           "ORDER BY LINK_NO, ADMISSION_DATE, RECORD_TYPE, DISCHARGE_DATE, ADMISSION, DISCHARGE, URI")
