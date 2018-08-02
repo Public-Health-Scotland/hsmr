a0b2ee6b78cc3b8cb0050469ff4b0c7bdf06f8de
David Caldwell <david.caldwell1@nhs.net>
2018-08-02 09:24:36 +0100

    Update trends sql with death query

--- R/sql_queries_trends.R
+++ R/sql_queries_trends.R
@@ -8,3 +8,8 @@ z_query_smr01_ltt <- paste("select LINK_NO, ADMISSION_DATE, DISCHARGE_DATE, CIS_
                            "ELSE 'NULL' END age_grp from SMR01_PI",
                            "where ADMISSION_DATE >= to_date(",z_start_date,", 'yyyy-MM-dd') AND ADMISSION_DATE <= to_date(",z_end_date,",'yyyy-MM-dd')",
                            "ORDER BY LINK_NO, ADMISSION_DATE, RECORD_TYPE, DISCHARGE_DATE, ADMISSION, DISCHARGE, URI")
+
+z_query_gro <- paste("select LINK_NO, DATE_OF_DEATH",
+                     "from ANALYSIS.GRO_DEATHS_C",
+                     "where DATE_OF_DEATH >= to_date(",z_start_date,",'yyyy-MM-dd')",
+                     "ORDER BY LINK_NO")
