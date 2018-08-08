be22f40252ee7f373225e53a271a44ff2c466424
annaprice <anna.price5@nhs.net>
2018-08-08 09:44:08 +0100

    Create data.R for data object documentation

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' Standardised Mortality Ratio (SMR) data for the quarterly HSMR publication.
+#'
+#' Data extracted from the SMR01 and GRO deaths databases.
+#'
+#' @format A tibble:
+#' \describe{
+#'   \item{variable name}{variable description}
+#' }
+#' @source
+#'
+#'
+#' @keywords
+
+#' Long term trends in SMR for the quarterly HSMR publication.
+#'
+#' @format A tibble:
+#' \describe{
+#'   \item{variable name}{variable description}
+#' }
+#' @source
+#'
+#' @keywords
