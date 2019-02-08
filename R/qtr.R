842ff38145883b8787afe7e06a9112c399b27114
Jack Hannah <jack.hannah1@nhs.net>
2019-02-08 09:58:04 +0000

    test push

--- R/qtr.R
+++ R/qtr.R
@@ -5,17 +5,17 @@
 
 # Current quarter
 qtr <- function(start, format = c("long", "short")) {
-  
+
   format <- match.arg(format)
-  
+
   if (format == "long") {
     return(paste0(as.character(lubridate::month(start,
                                                 label = TRUE,
-                                                abbr = FALSE)), 
+                                                abbr = FALSE)),
                   " to ",
                   format(zoo::as.yearmon(start + months(2)), "%B %Y")))
   }
-  
+
   if (format == "short") {
     return(paste0(as.character(lubridate::month(start,
                                                 label = TRUE,
@@ -23,9 +23,10 @@ qtr <- function(start, format = c("long", "short")) {
                   "-",
                   zoo::as.yearmon(start + months(2))))
   }
-  
+
 }
 
+# Tests
 qtr(lubridate::dmy(01012011), format = "short")
 qtr(lubridate::dmy(01102011), format = "long")
 
@@ -33,26 +34,27 @@ qtr(lubridate::dmy(01102011), format = "long")
 
 # Quarter end
 qtr_end <- function(start, quarter = c("current", "next")) {
-  
+
   quarter <- match.arg(quarter)
-  
+
   if (quarter == "current") {
     return(format(zoo::as.yearmon(start + months(2)), "%B %Y"))
   }
-  
+
   if (quarter == "next") {
     return(format(zoo::as.yearmon(start + months(5)), "%B %Y"))
   }
-  
+
 }
 
+# Tests
 qtr_end(lubridate::dmy(01072011), quarter = "current")
 qtr_end(lubridate::dmy(01042011), quarter = "next")
 
 
 # Previous quarter
 qtr_prev <- function(start) {
-  
+
   return(paste0(as.character(lubridate::month(start - months(3),
                                               label = TRUE,
                                               abbr = FALSE)),
@@ -60,7 +62,6 @@ qtr_prev <- function(start) {
                 format(zoo::as.yearmon(start - months(1)),"%B %Y")))
 }
 
+# Tests
 qtr_prev(dmy(01012018))
-
-
 
