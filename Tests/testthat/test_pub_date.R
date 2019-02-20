185002738cd3e25f949b5c27a0f29c8505d9987d
Jack Hannah <jack.hannah1@nhs.net>
2019-02-20 16:03:25 +0000

    add error tests

--- Tests/testthat/test_pub_date.R
+++ Tests/testthat/test_pub_date.R
@@ -6,11 +6,11 @@ context("pub_date")
 
 extract_end_date <- lubridate::dmy(30092018)
 
-test_that("Publication dates are correct for current, previous and next", {
-  expect_equal(lubridate::dmy(12022019),
-               pub_date(end_date = extract_end_date, pub = "current"))
+test_that("Publication dates are correct for previous, current and next", {
   expect_equal(lubridate::dmy(13112018),
                pub_date(end_date = extract_end_date, pub = "previous"))
+  expect_equal(lubridate::dmy(12022019),
+               pub_date(end_date = extract_end_date, pub = "current"))
   expect_equal(lubridate::dmy(14052019),
                pub_date(end_date = extract_end_date, pub = "next"))
 })
@@ -20,6 +20,7 @@ test_that("Errors if extract end date is not in date format", {
 })
 
 test_that("Errors if extract end date is not final day of a quarter", {
-  expect_error(pub_date(end_date = lubridate::dmy(30032018), pub = "current"))
-  expect_error(pub_date(end_date = lubridate::dmy(31092018), pub = "next"))
+  expect_error(pub_date(end_date = lubridate::dmy(01072018), pub = "previous"))
+  expect_error(pub_date(end_date = lubridate::dmy(31092018), pub = "current"))
+  expect_error(pub_date(end_date = lubridate::dmy(30122018), pub = "next"))
 })
