4a1af40120867ec9f97abc0159b6aa6b31c0e2b7
Jack Hannah <jack.hannah1@nhs.net>
2019-02-20 15:42:34 +0000

    add error handling tests

--- Tests/testthat/test_pub_date.R
+++ Tests/testthat/test_pub_date.R
@@ -13,4 +13,13 @@ test_that("Publication dates are correct for current, previous and next", {
                pub_date(end_date = extract_end_date, pub = "previous"))
   expect_equal(lubridate::dmy(14052019),
                pub_date(end_date = extract_end_date, pub = "next"))
-  })
+})
+
+test_that("Errors if extract end date is not in date format", {
+  expect_error(pub_date(end_date = "2019-09-30", pub = "current"))
+})
+
+test_that("Errors if extract end date is not final day of a quarter", {
+  expect_error(pub_date(end_date = lubridate::dmy(30032018), pub = "current"))
+  expect_error(pub_date(end_date = lubridate::dmy(31092018), pub = "next"))
+})
