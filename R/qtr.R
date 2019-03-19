87f2a3fef03fc8de12563a8f3fb1c2a10d18c08a
Jack Hannah <jack.hannah1@nhs.net>
2019-03-19 16:03:38 +0000

    error hadling for each function

--- R/qtr.R
+++ R/qtr.R
@@ -2,10 +2,9 @@
 #'
 #' @description
 #'
-# Needs error handling and documentation
+# Needs documentation
 # Possibly re-name functions to something more informative also
 
-# In each function, 'start' should be the first day of the quarter
 
 # Current quarter
 qtr <- function(first_day, format = c("long", "short")) {
@@ -47,35 +46,55 @@ qtr(lubridate::dmy(01102011), format = "long")
 
 #' @export
 # Quarter end
-qtr_end <- function(start, quarter = c("current", "next")) {
+qtr_end <- function(first_day, quarter = c("current", "next")) {
 
   quarter <- match.arg(quarter)
 
+  if (class(first_day) != "Date") {
+    stop("The first day of the quarter must be provided in date format")
+  }
+
+  if(!(format(first_day, "%d %B") %in% c("01 January", "01 April", "01 July",
+                                         "01 October"))) {
+    stop("The beginning of a quarter must be the first day of either January, ",
+         "April, September or December")
+  }
+
   if (quarter == "current") {
-    return(format(zoo::as.yearmon(start + months(2)), "%B %Y"))
+    return(format(zoo::as.yearmon(first_day + months(2)), "%B %Y"))
   }
 
   if (quarter == "next") {
-    return(format(zoo::as.yearmon(start + months(5)), "%B %Y"))
+    return(format(zoo::as.yearmon(first_day + months(5)), "%B %Y"))
   }
 
 }
 
 # Tests
 qtr_end(lubridate::dmy(01072011), quarter = "current")
-#qtr_end(lubridate::dmy(01042011), quarter = "next")
+qtr_end(lubridate::dmy(01042011), quarter = "next")
 
 #' @export
 # Previous quarter
-qtr_prev <- function(start) {
+qtr_prev <- function(first_day) {
 
-  return(paste0(as.character(lubridate::month(start - months(3),
+  if (class(first_day) != "Date") {
+    stop("The first day of the quarter must be provided in date format")
+  }
+
+  if(!(format(first_day, "%d %B") %in% c("01 January", "01 April", "01 July",
+                                         "01 October"))) {
+    stop("The beginning of a quarter must be the first day of either January, ",
+         "April, September or December")
+  }
+
+  return(paste0(as.character(lubridate::month(first_day - months(3),
                                               label = TRUE,
                                               abbr = FALSE)),
                 " to ",
-                format(zoo::as.yearmon(start - months(1)),"%B %Y")))
+                format(zoo::as.yearmon(first_day - months(1)),"%B %Y")))
 }
 
 # Tests
-#qtr_prev(dmy(01012018))
+qtr_prev(lubridate::dmy(01012018))
 
