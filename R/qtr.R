af6d190bd5ab7965de110f0b0b4734d8d8c2e7db
Jack Hannah <jack.hannah1@nhs.net>
2019-03-19 15:17:04 +0000

    error handling

--- R/qtr.R
+++ R/qtr.R
@@ -1,35 +1,48 @@
-#' @export
+#' @title Assign a date to a quarter
+#'
+#' @description
+#'
 # Needs error handling and documentation
 # Possibly re-name functions to something more informative also
 
 # In each function, 'start' should be the first day of the quarter
 
 # Current quarter
-qtr <- function(start, format = c("long", "short")) {
+qtr <- function(first_day, format = c("long", "short")) {
 
   format <- match.arg(format)
 
+  if (class(first_day) != "Date") {
+    stop("The first day of the quarter must be provided in date format")
+  }
+
+  if(!(format(first_day, "%d %B") %in% c("01 January", "01 April", "01 July",
+                                         "01 October"))) {
+    stop("The beginning of a quarter must be the first day of either January, ",
+         "April, September or December")
+  }
+
   if (format == "long") {
-    return(paste0(as.character(lubridate::month(start,
+    return(paste0(as.character(lubridate::month(first_day,
                                                 label = TRUE,
                                                 abbr = FALSE)),
                   " to ",
-                  format(zoo::as.yearmon(start + months(2)), "%B %Y")))
+                  format(zoo::as.yearmon(first_day + months(2)), "%B %Y")))
   }
 
   if (format == "short") {
-    return(paste0(as.character(lubridate::month(start,
+    return(paste0(as.character(lubridate::month(first_day,
                                                 label = TRUE,
                                                 abbr = TRUE)),
                   "-",
-                  zoo::as.yearmon(start + months(2))))
+                  zoo::as.yearmon(first_day + months(2))))
   }
 
 }
 
 # Tests
-#qtr(lubridate::dmy(01012011), format = "short")
-#qtr(lubridate::dmy(01102011), format = "long")
+qtr(lubridate::dmy(01012011), format = "short")
+qtr(lubridate::dmy(01102011), format = "long")
 
 
 #' @export
@@ -49,7 +62,7 @@ qtr_end <- function(start, quarter = c("current", "next")) {
 }
 
 # Tests
-#qtr_end(lubridate::dmy(01072011), quarter = "current")
+qtr_end(lubridate::dmy(01072011), quarter = "current")
 #qtr_end(lubridate::dmy(01042011), quarter = "next")
 
 #' @export
