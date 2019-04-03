08121054a0a54452cce8a9c547108f189a72089d
David Caldwell <david.caldwell1@nhs.net>
2019-04-03 11:58:00 +0100

    Create first draft of environment setup

---
+++ setup_environment.R
@@ -0,0 +1,55 @@
+#########################################################################
+# Name of file - setup_environment.R
+# Data release - Quarterly HSMR publication
+# Original Authors - David Caldwell
+# Orginal Date - April 2019
+#
+# Type - Reproducible Analytical Pipeline
+# Written/run on - RStudio server
+# Version of R - 3.2.3
+#
+# Description - Sets up environment required for running reproducible analytical
+# pipeline process. This is the only file which should require updating every
+# time the RAP process is run
+#
+# Approximate run time - xx minutes
+#########################################################################
+
+
+### 1 - Define Whether Running on Server or Locally ----
+# Comment out as appropriate
+platform <- c("server")
+#platform <- c("locally")
+
+
+# Define root directory for cl-out based on whether script is running locally or
+# on server
+plat_filepath <- if_else(platform == "server",
+                         '/conf/linkage/output/',
+                         '//stats/cl-out/')
+
+
+### 2 - Extract dates ----
+
+
+# Define the dates that the data are extracted from and to
+# Dates are in ddmmyyyy format
+
+
+# The beginning of baseline period/extract window
+start_date        <- dmy(01012016)
+
+# The beginning of baseline period/extract window for trend data
+start_date_trends <- dmy(01012014)
+
+# The end of the baseline period (for model)
+base_end          <- dmy(31122018)
+
+# Five years earlier for the five year look-back (pmorbs5)
+start_date_5      <- dmy(01012011)
+
+# End date for the cut off for data
+end_date          <- dmy(31122018)
+
+
+### END OF SCRIPT ###
