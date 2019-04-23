1ac71a979bb56e79139c542a3d816b461c2e5141
Jack Hannah <jack.hannah1@nhs.net>
2019-04-23 17:04:39 +0100

    be explicit about packages

--- R/smr_pmorbs.R
+++ R/smr_pmorbs.R
@@ -87,72 +87,75 @@ smr_pmorbs <- function(smr01, smr01_minus5, morbs){
   ### 2 - Creating Prior Morbidities ----
   # Vector of unique link numbers used for filtering below
   unique_id <- smr01 %>%
-    distinct(link_no) %>%
-    pull(link_no)
+    dplyr::distinct(link_no) %>%
+    dplyr::pull(link_no)
 
   # Create the following variables:
   # diag1   = ICD10 code for main condition to 3 and 4 digits, separated by an
   #           underscore
-  # pmorbs  = Charlson Index grouping (1-17) for main condition (0 if none apply)
+  # pmorbs  = Charlson Index grouping (1-17) for main condition
+  #           (0 if none apply)
   # pmorbs5_1 to pmorbs1_17 = initialise empty vectors for use in loop below
   # n_emerg                 = initialise empty vector for use in loop below
 
   smr01_minus5 %<>%
-    mutate(diag1 = paste(substr(main_condition, 1, 3),
-                         substr(main_condition, 1, 4),
-                         sep = "_")) %>%
+    dplyr::mutate(diag1 = paste(substr(main_condition, 1, 3),
+                                substr(main_condition, 1, 4),
+                                sep = "_")) %>%
 
     # Create the pmorbs variable using a join to the morbs dataset
-    fuzzy_left_join(select(morbs, pmorbs = morb, diag1_z = diag),
+    fuzzyjoin::fuzzy_left_join(dplyr::select(morbs,
+                                             pmorbs = morb,
+                                             diag1_z = diag),
                     by = c("diag1" = "diag1_z"),
-                    match_fun = str_detect) %>%
+                    match_fun = stringr::str_detect) %>%
 
     # Remove the joining variable
-    select(-ends_with("_z")) %>%
+    dplyr::select(-dplyr::ends_with("_z")) %>%
 
     # Replace cases with no match with zero
-    replace_na(list(pmorbs = 0)) %>%
-    mutate(pmorbs5_1  = 0,
-           pmorbs5_2  = 0,
-           pmorbs5_3  = 0,
-           pmorbs5_4  = 0,
-           pmorbs5_5  = 0,
-           pmorbs5_6  = 0,
-           pmorbs5_7  = 0,
-           pmorbs5_8  = 0,
-           pmorbs5_9  = 0,
-           pmorbs5_10 = 0,
-           pmorbs5_11 = 0,
-           pmorbs5_12 = 0,
-           pmorbs5_13 = 0,
-           pmorbs5_14 = 0,
-           pmorbs5_15 = 0,
-           pmorbs5_16 = 0,
-           pmorbs5_17 = 0,
-           pmorbs1_1  = 0,
-           pmorbs1_2  = 0,
-           pmorbs1_3  = 0,
-           pmorbs1_4  = 0,
-           pmorbs1_5  = 0,
-           pmorbs1_6  = 0,
-           pmorbs1_7  = 0,
-           pmorbs1_8  = 0,
-           pmorbs1_9  = 0,
-           pmorbs1_10 = 0,
-           pmorbs1_11 = 0,
-           pmorbs1_12 = 0,
-           pmorbs1_13 = 0,
-           pmorbs1_14 = 0,
-           pmorbs1_15 = 0,
-           pmorbs1_16 = 0,
-           pmorbs1_17 = 0,
-           n_emerg    = 0) %>%
+    tidyr::replace_na(list(pmorbs = 0)) %>%
+    dplyr::mutate(pmorbs5_1  = 0,
+                  pmorbs5_2  = 0,
+                  pmorbs5_3  = 0,
+                  pmorbs5_4  = 0,
+                  pmorbs5_5  = 0,
+                  pmorbs5_6  = 0,
+                  pmorbs5_7  = 0,
+                  pmorbs5_8  = 0,
+                  pmorbs5_9  = 0,
+                  pmorbs5_10 = 0,
+                  pmorbs5_11 = 0,
+                  pmorbs5_12 = 0,
+                  pmorbs5_13 = 0,
+                  pmorbs5_14 = 0,
+                  pmorbs5_15 = 0,
+                  pmorbs5_16 = 0,
+                  pmorbs5_17 = 0,
+                  pmorbs1_1  = 0,
+                  pmorbs1_2  = 0,
+                  pmorbs1_3  = 0,
+                  pmorbs1_4  = 0,
+                  pmorbs1_5  = 0,
+                  pmorbs1_6  = 0,
+                  pmorbs1_7  = 0,
+                  pmorbs1_8  = 0,
+                  pmorbs1_9  = 0,
+                  pmorbs1_10 = 0,
+                  pmorbs1_11 = 0,
+                  pmorbs1_12 = 0,
+                  pmorbs1_13 = 0,
+                  pmorbs1_14 = 0,
+                  pmorbs1_15 = 0,
+                  pmorbs1_16 = 0,
+                  pmorbs1_17 = 0,
+                  n_emerg    = 0) %>%
 
     # In order to increase the efficiency of the following for loop:
     # Only keep records with link numbers which appear in the main extract
     # (smr01)
 
-    filter(link_no %in% unique_id)
+    dplyr::filter(link_no %in% unique_id)
 
   # For every row in the pmorbs extract, look at each of the prior 50 rows and
   # IF the previous episode belongs to the same person
@@ -165,7 +168,7 @@ smr_pmorbs <- function(smr01, smr01_minus5, morbs){
   # NOTE: This section of code uses the data.table package rather than dplyr
 
   # convert tibble to data.table format
-  smr01_minus5 <- data.table(smr01_minus5)
+  smr01_minus5 <- data.table::data.table(smr01_minus5)
 
   for(i in 1:50) {
 
@@ -177,79 +180,114 @@ smr_pmorbs <- function(smr01, smr01_minus5, morbs){
     #                 admission
     # old_pmorbs    = the pmorbs group the ith previous record is assigned to
     # old_link      = the link number of the ith previous record
-    smr01_minus5[, `:=` (old_admission = (admission_date -
-                                            shift(admission_date, i))/60/60/24,
-                         old_pmorbs = shift(pmorbs, i),
-                         old_link = shift(link_no, i))]
+    smr01_minus5[, `:=` (old_admission =
+                           (admission_date - data.table::shift(admission_date,
+                                                               i))/60/60/24,
+                         old_pmorbs = data.table::shift(pmorbs, i),
+                         old_link = data.table::shift(link_no, i))]
 
     smr01_minus5[admission_date >= start_date & old_pmorbs == 1  &
-                   old_admission <= 1825 & old_link == link_no, pmorbs5_1 := 5]
+                   old_admission <= 1825 & old_link == link_no,
+                 pmorbs5_1 := 5]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 2 &
-                   old_admission <= 1825 & old_link == link_no, pmorbs5_2 := 11]
+                   old_admission <= 1825 & old_link == link_no,
+                 pmorbs5_2 := 11]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 3 &
-                   old_admission <= 1825 & old_link == link_no, pmorbs5_3 := 13]
+                   old_admission <= 1825 & old_link == link_no,
+                 pmorbs5_3 := 13]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 4 &
-                   old_admission <= 1825 & old_link == link_no, pmorbs5_4 := 4]
+                   old_admission <= 1825 & old_link == link_no,
+                 pmorbs5_4 := 4]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 5 &
-                   old_admission <= 1825 & old_link == link_no, pmorbs5_5 := 14]
+                   old_admission <= 1825 & old_link == link_no,
+                 pmorbs5_5 := 14]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 6 &
-                   old_admission <= 1825 & old_link == link_no, pmorbs5_6 := 3]
+                   old_admission <= 1825 & old_link == link_no,
+                 pmorbs5_6 := 3]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 7 &
-                   old_admission <= 1825 & old_link == link_no, pmorbs5_7 := 8]
+                   old_admission <= 1825 & old_link == link_no,
+                 pmorbs5_7 := 8]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 8 &
-                   old_admission <= 1825 & old_link == link_no, pmorbs5_8 := 9]
+                   old_admission <= 1825 & old_link == link_no,
+                 pmorbs5_8 := 9]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 9 &
-                   old_admission <= 1825 & old_link == link_no, pmorbs5_9 := 6]
+                   old_admission <= 1825 & old_link == link_no,
+                 pmorbs5_9 := 6]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 10 &
-                   old_admission <= 1825 & old_link == link_no, pmorbs5_10 := 4]
+                   old_admission <= 1825 & old_link == link_no,
+                 pmorbs5_10 := 4]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 11 &
-                   old_admission <= 1825 & old_link == link_no, pmorbs5_11 := 8]
+                   old_admission <= 1825 & old_link == link_no,
+                 pmorbs5_11 := 8]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 12 &
-                   old_admission <= 1825 & old_link == link_no, pmorbs5_12 := -1]
+                   old_admission <= 1825 & old_link == link_no,
+                 pmorbs5_12 := -1]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 13 &
-                   old_admission <= 1825 & old_link == link_no, pmorbs5_13 := 1]
+                   old_admission <= 1825 & old_link == link_no,
+                 pmorbs5_13 := 1]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 14 &
-                   old_admission <= 1825 & old_link == link_no, pmorbs5_14 := 10]
+                   old_admission <= 1825 & old_link == link_no,
+                 pmorbs5_14 := 10]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 15 &
-                   old_admission <= 1825 & old_link == link_no, pmorbs5_15 := 14]
+                   old_admission <= 1825 & old_link == link_no,
+                 pmorbs5_15 := 14]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 16 &
-                   old_admission <= 1825 & old_link == link_no, pmorbs5_16 := 18]
+                   old_admission <= 1825 & old_link == link_no,
+                 pmorbs5_16 := 18]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 17 &
-                   old_admission <= 1825 & old_link == link_no, pmorbs5_17 := 2]
+                   old_admission <= 1825 & old_link == link_no,
+                 pmorbs5_17 := 2]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 1 &
-                   old_admission <= 365 & old_link == link_no, pmorbs1_1 := 5]
+                   old_admission <= 365 & old_link == link_no,
+                 pmorbs1_1 := 5]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 2 &
-                   old_admission <= 365 & old_link == link_no, pmorbs1_2 := 11]
+                   old_admission <= 365 & old_link == link_no,
+                 pmorbs1_2 := 11]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 3 &
-                   old_admission <= 365 & old_link == link_no, pmorbs1_3 := 13]
+                   old_admission <= 365 & old_link == link_no,
+                 pmorbs1_3 := 13]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 4 &
-                   old_admission <= 365 & old_link == link_no, pmorbs1_4 := 4]
+                   old_admission <= 365 & old_link == link_no,
+                 pmorbs1_4 := 4]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 5 &
-                   old_admission <= 365 & old_link == link_no, pmorbs1_5 := 14]
+                   old_admission <= 365 & old_link == link_no,
+                 pmorbs1_5 := 14]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 6 &
-                   old_admission <= 365 & old_link == link_no, pmorbs1_6 := 3]
+                   old_admission <= 365 & old_link == link_no,
+                 pmorbs1_6 := 3]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 7 &
-                   old_admission <= 365 & old_link == link_no, pmorbs1_7 := 8]
+                   old_admission <= 365 & old_link == link_no,
+                 pmorbs1_7 := 8]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 8 &
-                   old_admission <= 365 & old_link == link_no, pmorbs1_8 := 9]
+                   old_admission <= 365 & old_link == link_no,
+                 pmorbs1_8 := 9]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 9 &
-                   old_admission <= 365 & old_link == link_no, pmorbs1_9 := 6]
+                   old_admission <= 365 & old_link == link_no,
+                 pmorbs1_9 := 6]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 10 &
-                   old_admission <= 365 & old_link == link_no, pmorbs1_10 := 4]
+                   old_admission <= 365 & old_link == link_no,
+                 pmorbs1_10 := 4]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 11 &
-                   old_admission <= 365 & old_link == link_no, pmorbs1_11 := 8]
+                   old_admission <= 365 & old_link == link_no,
+                 pmorbs1_11 := 8]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 12 &
-                   old_admission <= 365 & old_link == link_no, pmorbs1_12 := -1]
+                   old_admission <= 365 & old_link == link_no,
+                 pmorbs1_12 := -1]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 13 &
-                   old_admission <= 365 & old_link == link_no, pmorbs1_13 := 1]
+                   old_admission <= 365 & old_link == link_no,
+                 pmorbs1_13 := 1]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 14 &
-                   old_admission <= 365 & old_link == link_no, pmorbs1_14 := 10]
+                   old_admission <= 365 & old_link == link_no,
+                 pmorbs1_14 := 10]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 15 &
-                   old_admission <= 365 & old_link == link_no, pmorbs1_15 := 14]
+                   old_admission <= 365 & old_link == link_no,
+                 pmorbs1_15 := 14]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 16 &
-                   old_admission <= 365 & old_link == link_no, pmorbs1_16 := 18]
+                   old_admission <= 365 & old_link == link_no,
+                 pmorbs1_16 := 18]
     smr01_minus5[admission_date >= start_date & old_pmorbs == 17 &
-                   old_admission <= 365 & old_link == link_no, pmorbs1_17 := 2]
+                   old_admission <= 365 & old_link == link_no,
+                 pmorbs1_17 := 2]
 
   }
 
@@ -258,30 +296,36 @@ smr_pmorbs <- function(smr01, smr01_minus5, morbs){
   # and 5 years prior to admission
   # smr01_minus5 will be automatically converted back to a tibble here
   smr01_minus5 %<>%
-    mutate(pmorbs1_12 = replace(pmorbs1_12, pmorbs1_12 == -1 & pmorbs1_6 == 0,
-                                2),
-           pmorbs5_12 = replace(pmorbs5_12, pmorbs5_12 == -1 & pmorbs5_6 == 0,
-                                2),
-           pmorbs1_11 = replace(pmorbs1_11, pmorbs1_15 == 14 & pmorbs1_11 == 8,
-                                0),
-           pmorbs5_11 = replace(pmorbs5_11, pmorbs5_15 == 14 & pmorbs5_11 == 8,
-                                0)) %>%
-    mutate(pmorbs1_sum = rowSums(select(., starts_with("pmorbs1")))) %>%
-    mutate(pmorbs5_sum = rowSums(select(., starts_with("pmorbs5")))) %>%
-    group_by(link_no, cis_marker) %>%
-    mutate_at(vars(ends_with("_sum")), max) %>%
+    dplyr::mutate(pmorbs1_12 = replace(pmorbs1_12,
+                                       pmorbs1_12 == -1 & pmorbs1_6 == 0,
+                                       2),
+                  pmorbs5_12 = replace(pmorbs5_12,
+                                       pmorbs5_12 == -1 & pmorbs5_6 == 0,
+                                       2),
+                  pmorbs1_11 = replace(pmorbs1_11,
+                                       pmorbs1_15 == 14 & pmorbs1_11 == 8,
+                                       0),
+                  pmorbs5_11 = replace(pmorbs5_11,
+                                       pmorbs5_15 == 14 & pmorbs5_11 == 8,
+                                       0)) %>%
+    dplyr::mutate(pmorbs1_sum = rowSums(
+      dplyr::select(., dplyr::starts_with("pmorbs1")))) %>%
+    dplyr::mutate(pmorbs5_sum = rowSums(
+      dplyr::select(., dplyr::starts_with("pmorbs5")))) %>%
+    dplyr::group_by(link_no, cis_marker) %>%
+    dplyr::mutate_at(dplyr::vars(dplyr::ends_with("_sum")), max) %>%
 
     # Add epinum to filter down to first episode within a CIS for the
     # calculation of the number of previous emergency admissions
-    mutate(epinum = row_number()) %>%
-    ungroup() %>%
-    filter(epinum == 1) %>%
-    mutate(n_emerg = 0)
+    dplyr::mutate(epinum = row_number()) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(epinum == 1) %>%
+    dplyr::mutate(n_emerg = 0)
 
 
   # Convert back to a data.table for the number of previous emergency
   # admissions
-  smr01_minus5 <- data.table(smr01_minus5)
+  smr01_minus5 <- data.table::data.table(smr01_minus5)
 
 
   ### 3 - Previous emergency admissions ----
@@ -297,23 +341,25 @@ smr_pmorbs <- function(smr01, smr01_minus5, morbs){
 
     # 1:50 because the 95th percentile of episode counts per patient was 51
 
-    smr01_minus5[, `:=`(old_admission = (admission_date -
-                                           shift(admission_date, i))/60/60/24,
-                        old_tadm = shift(old_smr1_tadm_code, i),
-                        old_link = shift(link_no, i))]
+    smr01_minus5[, `:=`(old_admission =
+                          (admission_date - data.table::shift(admission_date,
+                                                              i))/60/60/24,
+                        old_tadm = data.table::shift(old_smr1_tadm_code, i),
+                        old_link = data.table::shift(link_no, i))]
 
     smr01_minus5[admission_date >= start_date & old_link == link_no &
-                   old_tadm >= 4 & old_admission <= 365, n_emerg := n_emerg + 1]
+                   old_tadm >= 4 & old_admission <= 365,
+                 n_emerg := n_emerg + 1]
 
   }
 
   # Select required variables from smr01_minus5
   smr01_minus5 %<>%
-    select(link_no, cis_marker, pmorbs1_sum, pmorbs5_sum, n_emerg)
+    dplyr::select(link_no, cis_marker, pmorbs1_sum, pmorbs5_sum, n_emerg)
 
   # Join smr01_minus5 on to the main tibble
   smr01 %<>%
-    left_join(smr01_minus5, by = c("link_no", "cis_marker"))
+    dplyr::left_join(smr01_minus5, by = c("link_no", "cis_marker"))
 
   return(smr01)
 
