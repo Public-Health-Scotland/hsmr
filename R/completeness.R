5aa96a922983e99b33e4306af6b7c7532e6ccf55
Jack Hannah <jack.hannah1@nhs.net>
2019-05-22 16:26:13 +0100

    add comments

--- R/completeness.R
+++ R/completeness.R
@@ -24,15 +24,27 @@ completeness <- function(quarter = c("previous", "current"),
 
   tmp <- tempfile(fileext = ".xlsx")
 
+  # Note that this file can occasionally change when a new quarter's data is
+  # added, which will cause the function to error if not updated
   httr::GET(url = paste0("https://www.isdscotland.org/products-and-Services/",
                          "Data-Support-and-Monitoring/SMR-Completeness/_docs/",
                          "SMR_Estimates.xlsx"),
             httr::write_disk(tmp))
 
+  # Note that the range is based on B29 being "NHS Board" and B47 being "All
+  # NHS Boards" - if these shift up or down slightly when the file is modified
+  # then the range will need to be updated accordingly for the function to work
+  # as intended
   comp <- suppressMessages(
     readxl::read_xlsx(tmp, range = "B29:AF47", col_names = FALSE)) %>%
     janitor::clean_names()
 
+  # The above step parses the file with the dataset names in the first row,
+  # however it only adds the dataset name to the column containing the first
+  # quarter of data, and gives all subsuquent quarters NA
+  #
+  # This step replace those NAs in the first row with the nearest non-NA value
+  # to the left, which should be the name of the relevant dataset
   comp[1,] <- t(dplyr::select(
     tidyr::fill(
       tidyr::gather(
@@ -40,23 +52,34 @@ completeness <- function(quarter = c("previous", "current"),
           comp, 1)),
       value), 2))
 
+  # Set the dataset names as the column names
   comp %<>%
     setNames(., unlist(dplyr::slice(., 1), use.names = FALSE)) %>%
     dplyr::slice(-1) %>%
     janitor::clean_names() %>%
+
+    # Subsequently select only the columns pertaining to the name of the board
+    # and the SMR01 dataset
+    # The regex is needed to differentiate SMR01 from SMR01 GLS
     dplyr::select(nhs_board, smr01, dplyr::matches("^smr01_[0-9]*$"))
 
+  # Now set the column names (with the exception of the one pertaining to the
+  # name of the board) to the relevant quarter
   colnames(comp)[-1] <- unlist(dplyr::slice(dplyr::select(comp, -nhs_board), 1),
                                use.names = FALSE)
 
   comp %<>%
     dplyr::slice(-1) %>%
     janitor::clean_names() %>%
+
+    # Select only the two most recent quarters
     dplyr::select(nhs_board, tail(names(.), 2)) %>%
     dplyr::mutate(nhs_board = replace(nhs_board,
                                       nhs_board == "All NHS Boards",
                                       "Scotland"))
 
+  # Calculate the final month in the most recent quarter, to ensure that the
+  # function can only be used on the two most recent quarters
   last_month <- colnames(comp)[2:3]
 
   last_month <- unlist(stringr::str_split(last_month, "_"))
@@ -85,9 +108,15 @@ completeness <- function(quarter = c("previous", "current"),
   if (level == "board") {
 
     comp %<>%
+
+      # Remove the row pertaining to Scotland level data
       dplyr::slice(-dplyr::n()) %>%
+
+      # Filter boards which had a completeness level < 95%
       dplyr::filter_at(dplyr::vars(dplyr::last_col()),
                        dplyr::any_vars(. < 0.95)) %>%
+
+      # Present SMR01 completeness as a percentage
       dplyr::mutate_at(dplyr::vars(dplyr::last_col()),
                        function(x) paste0("(",
                                           scales::percent(as.numeric(x),
@@ -99,6 +128,7 @@ completeness <- function(quarter = c("previous", "current"),
 
     if (length(comp) == 0) {
 
+      # If all boards have >= 95% completeness, return a sentence saying that
       return(capture.output(
         cat("All NHS Board HSMRs are based on completeness levels of 95% and",
             "above for",
@@ -108,6 +138,8 @@ completeness <- function(quarter = c("previous", "current"),
                                      format = "long")))))
     } else {
 
+      # If >= 1 board has < 95% completeness, return a sentence which lists
+      # those boards accompanied by their completeness percentage in brackets
       return(capture.output(
         cat("All NHS Board HSMRs are based on completeness levels of 95% and",
             "above for",
@@ -127,7 +159,11 @@ completeness <- function(quarter = c("previous", "current"),
   if (level == "scotland") {
 
     comp %<>%
+
+      # Select only the row pertaining to Scotland level data
       dplyr::slice(dplyr::n()) %>%
+
+      # Present Scotland's SMR01 completeness as a percentage
       dplyr::mutate_at(dplyr::vars(dplyr::last_col()),
                        function(x) scales::percent(as.numeric(x),
                                                    accuracy = 1)) %>%
