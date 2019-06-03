47c68ac19703fc352d83a680e31cf6b9a18e34d1
Jack Hannah <jack.hannah1@nhs.net>
2019-06-03 13:36:20 +0100

    first test done

--- tests/testthat/test-completeness.R
+++ tests/testthat/test-completeness.R
@@ -1,23 +1,42 @@
 library(hsmr)
 context("completeness")
 
-test_that("Less than 95%", {
-  j <- as.numeric(
+# qtr_start is calculated in the helper script
+
+test_that("Output returns boards only with SMR01 completeness < 95%", {
+
+  # Extract all numbers from the returned output for both the current and
+  # previous quarters
+  # Remove the first two as these should always be '95%' and the year
+  numbers_current <- as.numeric(
     tail(
       unlist(
         stringr::str_match_all(
           hsmr::completeness("current",
                              "board",
-                             first_day),
+                             qtr_start),
           "[0-9]+")), -2))
 
-  k <- dplyr::case_when(
-    length(j) > 0 ~ j,
-    length(j) == 0 ~ 0
-  )
+  numbers_prev <- as.numeric(
+    tail(
+      unlist(
+        stringr::str_match_all(
+          hsmr::completeness("previous",
+                             "board",
+                             qtr_start),
+          "[0-9]+")), -2))
 
-  for (i in 1:length(k)) {
-    expect_lt(k[i], 95)
+  # If no boards have SMR01 completeness < 95%, set equal to zero, so the tests
+  # pass
+  numbers_current <- replace(numbers_current, length(numbers_current) == 0, 0)
+  numbers_prev <- replace(numbers_prev, length(numbers_prev) == 0, 0)
+
+  # Check that every returned percentage is < 95
+  for (i in 1:length(numbers_current)) {
+    expect_lt(numbers_current[i], 95)
+  }
+
+  for (j in 1:length(numbers_prev)) {
+    expect_lt(numbers_prev[j], 95)
   }
 })
-
