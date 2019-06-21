c00e6903ba7714ec6bf79cb2a7a75c9e7bd4b2ff
David Caldwell <david.caldwell1@nhs.net>
2019-06-21 11:07:19 +0100

    Add expect error test for date

--- tests/testthat/test-yr.R
+++ tests/testthat/test-yr.R
@@ -9,3 +9,11 @@ test_that("Years are correct for previous, current and next publications", {
   expect_equal("July 2018 to June 2019",
                yr(end_date = lubridate::dmy(30062019)))
 })
+
+test_that("Errors if extract end date is not in date format", {
+  expect_error(yr(end_date = "2019-09-30"))
+  expect_error(yr(end_date = as.factor("2019-09-30")))
+  expect_error(yr(end_date = as.numeric(lubridate::dmy(31032018))))
+  expect_error(yr(end_date = as.integer(lubridate::dmy(31032018))))
+  expect_error(yr(end_date = as.character(lubridate::dmy(31032019))))
+})
