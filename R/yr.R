7bc2f99cd2c25a516330976585f86242c7c19c5f
David Caldwell <david.caldwell1@nhs.net>
2019-06-21 10:19:47 +0100

    Add error handling to yr()

--- R/yr.R
+++ R/yr.R
@@ -16,6 +16,18 @@
 #' @rdname yr
 yr <- function(end_date){
 
+  if (class(end_date) != "Date") {
+    stop("The extract end date must be provided in date format")
+  }
+
+  if(!(format(end_date, "%d %B") %in% c("31 March",
+                                        "30 June",
+                                        "30 September",
+                                        "31 December"))) {
+    stop("The extract end date must be the final day of either March, June, ",
+         "September or December")
+  }
+
   paste0(lubridate::month(end_date - lubridate::years(1) + lubridate::days(1),
                label = TRUE, abbr = FALSE),
          " ",
