7652ff5ab7d7c9de52a12cd2f4a95c4b6fa40726
Jack Hannah <jack.hannah1@nhs.net>
2019-06-25 14:55:43 +0100

    add error handling

--- R/mit_available.R
+++ R/mit_available.R
@@ -1,5 +1,17 @@
 mit_available <- function(end_date) {
 
+  if (class(end_date) != "Date") {
+    stop("The extract end date must be provided in date format")
+  }
+
+  if(!(format(end_date, "%d %B") %in% c("31 March",
+                                        "30 June",
+                                        "30 September",
+                                        "31 December"))) {
+    stop("The extract end date must be the final day of either March, June, ",
+         "September or December")
+  }
+
   first <- lubridate::floor_date(hsmr::pub_date(end_date = end_date,
                                                 pub = "current"),
                                  unit = "month") - months(1)
@@ -23,3 +35,4 @@ mit_available(end_date)
 pub_date(end_date, "previous")
 
 mit_available(end_date = lubridate::dmy(31122018))
+
