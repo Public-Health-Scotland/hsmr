e15b2f79d02c581554776b00598b053fc37d8299
Jack Hannah <jack.hannah1@nhs.net>
2019-06-26 11:36:56 +0100

    add format tests

--- tests/testthat/test-mit_available.R
+++ tests/testthat/test-mit_available.R
@@ -11,3 +11,10 @@ test_that("Returns date of last Friday in the month before publication", {
   expect_equal(lubridate::dmy(25102019),
                mit_available(end_date = lubridate::dmy(30062019)))
 })
+
+test_that("Errors if extract end date is not in date format", {
+  expect_error(mit_available(end_date = "2019-09-30"))
+  expect_error(mit_available(end_date = as.factor("2019-09-30")))
+  expect_error(mit_available(end_date = as.numeric(lubridate::dmy(31032018))))
+  expect_error(mit_available(end_date = as.integer(lubridate::dmy(31032018))))
+})
