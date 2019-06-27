08df973b886953fdd239beb16bb02e86ca4480c0
Jack Hannah <jack.hannah1@nhs.net>
2019-06-27 11:24:08 +0100

    add error handling

--- R/file_sizes.R
+++ R/file_sizes.R
@@ -1,7 +1,17 @@
 file_sizes <- function(filepath = here::here("data", "output")) {
 
+  if (!file.exists(filepath)) {
+    stop(paste0("A valid filepath to the folder containing the HSMR Excel ",
+                "tables must be supplied"))
+  }
+
   x <- dir(path = filepath,
            pattern = "^\\d{4}-\\d{2}-\\d{2}(T|t)able[a-zA-Z0-9\\_\\-]*\\.xlsx$")
+
+  if (length(x) != 3) {
+    stop(paste0("Exactly three HSMR Excel tables should be present in the ",
+                "folder: Tables 1, 2 and 3"))
+  }
 
   x %>%
     purrr::map(~file.info(paste0(filepath, "/", .))$size) %>%
@@ -11,6 +21,3 @@ file_sizes <- function(filepath = here::here("data", "output")) {
     trimws() %>%
     paste0("Excel ", .)
 }
-
-
-
