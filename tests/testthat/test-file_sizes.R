dd07953fa945a8a1f2bcc17114477eab668a9bed
Jack Hannah <jack.hannah1@nhs.net>
2019-07-03 16:06:05 +0100

    add error tests for end_date

--- tests/testthat/test-file_sizes.R
+++ tests/testthat/test-file_sizes.R
@@ -15,7 +15,18 @@ test_that("File sizes are returned in KB or MB", {
 test_that("Errors if extract end date is not in date format", {
   expect_error(file_sizes(end_date = "2019-03-31"))
   expect_error(file_sizes(end_date = as.factor("2019-03-31")))
-  expect_error(file_sizes(end_date = as.numeric(lubridate::dmy(31032019))))
-  expect_error(file_sizes(end_date = as.integer(lubridate::dmy(31032019))))
+  expect_error(file_sizes(end_date = as.numeric(extract_end_date)))
+  expect_error(file_sizes(end_date = as.integer(extract_end_date)))
 })
 
+test_that("Errors if extract end date is not final day of a quarter", {
+  expect_error(file_sizes(end_date = lubridate::dmy(01072019)))
+  expect_error(file_sizes(end_date = lubridate::dmy(15071998)))
+  expect_error(file_sizes(end_date = lubridate::dmy(30041989)))
+})
+
+test_that("Errors if extract end date is before 2019-03-31", {
+  expect_error(file_sizes(end_date = lubridate::dmy(31122018)))
+  expect_error(file_sizes(end_date = lubridate::dmy(31032018)))
+  expect_error(file_sizes(end_date = lubridate::dmy(30061997)))
+})
