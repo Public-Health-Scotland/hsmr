74cc25589be2df3f7e480664b06cce60cecf3804
David Caldwell <david.caldwell1@nhs.net>
2019-08-05 14:56:01 +0100

    Initial draft of NHS Performs

---
+++ R/nhs_performs.R
@@ -0,0 +1,108 @@
+#' @title NHS Performs
+#'
+#' @description \code{nhs_performs} reformats the output from \code(smr_data)
+#' and \code{trend_data} in order to fit the template agreed with the team in
+#' charge of NHS Performs. It adds variables \code{topic} and \code{indicator}
+#' and reformats \code{period}, dropping all other variables except
+#' \code{location_name} and \code{smr}/\code{crd_rat}.
+#'
+#' @details NHS Performs is a companion website to the ISD website
+#'
+#' @param data The input \code{tibble} which is the output from \code{smr_data}
+#' or \code{create_trends}.
+#' @param indicator controls whether the function reformats data for the trends
+#' information tab or the smr information tab.
+#'
+#' @examples
+#' nhs_performs(data = smr_data, indicator = "HSMR")
+#' nhs_performs(data = trend_data, indicator = "Crude")
+#'
+#' @export
+
+nhs_performs <- function(input_data, end_date, indicator = c("HSMR", "Crude")){
+
+  match.arg(indicator)
+
+  if (class(end_date) != "Date") {
+    stop("The extract end date must be provided in date format")
+  }
+
+  if(!(format(end_date, "%d %B") %in% c("31 March",
+                                        "30 June",
+                                        "30 September",
+                                        "31 December"))) {
+    stop("The extract end date must be the final day of either March, June, ",
+         "September or December")
+  }
+
+
+  if(indicator == "HSMR"){
+    if(!all(c("period",	"deaths",	"pred",	"pats",	"smr",
+                                  "crd_rate",	"location_type",	"location",
+                                  "hb",	"location_name",	"completeness_date",
+                                  "period_label",	"death_scot",	"pred_scot",
+                                  "pats_scot",	"smr_scot")
+            %in% names(input_data))){
+
+      stop(paste0("For NHS Performs data for HSMR, input_data object must be ",
+                  "output from smr_data()"))}
+
+    input_data %<>%
+      filter(period == 3 & location %in%
+               c("C206H", "C418H", "Y104H", "Y146H", "N101H", "A101H", "R101H",
+                 "H212H", "B120H", "H103H", "N411H", "Y146H", "V217H", "Y144H",
+                 "Z102H", "G107H", "D102H", "C313H", "C121H", "G306H", "T101H",
+                 "T202H", "G405H", "F805H", "H202H", "C418H", "S314H", "S308H",
+                 "G207H", "T312H", "A210H", "A111H", "L302H", "L106H", "L308H",
+                 "F704H", "S116H", "W107H", "G516H", "Scot")) %>%
+      mutate(topic       = "HSMR",
+             indicator   = "SMR",
+             time_period = end_date - years(1) + days(1)) %>%
+      select(topic, indicator, time_period, location, location_name, smr,
+             completeness_date)
+
+    return(input_data)
+
+
+  }
+
+  if(indicator == "Crude"){
+    if(!all(c("hb", "location",	"location_name",
+                                   "agg_label",	"quarter",	"quarter_short",
+                                   "quarter_full",	"sub_grp",	"label",
+                                   "scot_deaths",	"scot_pats",
+                                   "completeness_date",	"deaths",	"pats",
+                                   "crd_rate"))
+       %in% names(input_data)){
+
+      stop(paste0("For NHS Performs data for Crude Rates, input_data object ",
+                  "must be ",
+                  "output from create_trends()"))}
+
+    input_data %<>%
+      filter(sub_grp == "All Admissions" & (agg_label == "Board" | agg_label ==
+                                              "Scotland")) %>%
+      mutate(topic       = "HSMR",
+             indicator   = "Crude Mortality",
+             time_period = case_when(substr(quarter_short, 1, 3) == "Apr" ~
+                                       paste0("01/04/", substr(quarter_short,
+                                                               9, 12)),
+                                     substr(quarter_short, 1, 3) == "Jul" ~
+                                       paste0("01/07/", substr(quarter_short,
+                                                               9, 12)),
+                                     substr(quarter_short, 1, 3) == "Oct" ~
+                                       paste0("01/10/", substr(quarter_short,
+                                                               9, 12)),
+                                     substr(quarter_short, 1, 3) == "Jan" ~
+                                       paste0("01/01/", substr(quarter_short,
+                                                               9, 12)))) %>%
+      select(topic, indicator, time_period, location, location_name, crd_rate,
+             completeness_date)
+
+    return(input_data)
+
+  }
+
+
+
+}
