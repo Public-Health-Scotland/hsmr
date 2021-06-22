ae41d857f1a356b3c1da50e9b80115d8eef08e80
Jaime Villacampa <jaime.villacampa@phs.scot>
2021-06-22 12:45:01 +0100

    function to save files

---
+++ R/save_file.R
@@ -0,0 +1,43 @@
+#' @title Save files for different types of files and for development work
+#'
+#' @description Save files for different types of files and for development work.
+#'
+#' @param dataset The object that you want to save.
+#' @param filename A \code{character} string with name of output file.
+#' @param outfolder A \code{character} string with name of destination folder.
+#' This can be "base_file" or "output".
+#' @param type A \code{character} string with name of file extension. 
+#' This can be csv, rds or xlsx
+#' @param dev A \code{character} string. It add a "dev_version" bit to the filename
+#' to be able to identify development work
+#'
+#' @export
+
+save_file <- function(dataset, filename, out_folder = c("base_file", "output"), 
+                      type = c("csv", "rds", "xlsx"), dev = F) {
+  
+  dataset <- dataset # brings data object
+  
+  dash <- case_when(type == "xlsx" ~ "-", T ~ "_")
+  
+  # Creating dev version of files if required
+  if (dev == FALSE) {
+    filepath <- paste0(data_folder, pub_day, "/", out_folder, "/",
+                       pub_day, dash, filename, ".", type)
+  } else if (dev == TRUE) {
+    filepath <- paste0(data_folder, pub_day,  "/", out_folder, "/",
+                       pub_day, dash, filename, "_dev_version.", type)
+  }
+  
+  # Saving file depending on type
+  if (type == "csv") {
+    readr::write_csv(dataset, filepath)
+  } else if (type == "rds") {
+    saveRDS(dataset, filepath)
+    
+  } else if (type == "xlsx") {
+    openxlsx::saveWorkbook(dataset, filepath, overwrite = TRUE)
+  }
+  
+}
+
