fe950cc3e837efcc99e048970eb56e5faea92678
ScottMG-PHS <scott.mclaren-gow@phs.scot>
2021-07-14 16:19:54 +0100

    Truncate TDE sheet names to 31 characters
    That is the maximum excel/openxlsx can support

--- R/save_file.R
+++ R/save_file.R
@@ -6,7 +6,7 @@
 #' @param filename A \code{character} string with name of output file.
 #' @param outfolder A \code{character} string with name of destination folder.
 #' This can be "base_file", "tde" or "output".
-#' @param type A \code{character} string with name of file extension. 
+#' @param type A \code{character} string with name of file extension.
 #' This can be csv, rds or xlsx
 #' @param dev A \code{character} string. It add a "dev_version" bit to the filename
 #' to be able to identify development work
@@ -15,13 +15,13 @@
 #' @export
 
 
-save_file <- function(dataset, filename, out_folder = c("base_file", "output", "tde", "open_data"), 
+save_file <- function(dataset, filename, out_folder = c("base_file", "output", "tde", "open_data"),
                       type = c("csv", "rds", "xlsx"), dev = F, overwrite) {
-  
+
   dataset <- dataset # brings data object
-  
+
   dash <- case_when(type == "xlsx" ~ "-", T ~ "_")
-  
+
   # Creating dev version of files if required
   if (dev == FALSE) {
     if (out_folder == "tde") {
@@ -31,31 +31,33 @@ save_file <- function(dataset, filename, out_folder = c("base_file", "output", "
       filepath <- paste0(data_folder, pub_day, "/", out_folder, "/",
                          pub_day, dash, filename, ".", type)
     }
-    
+
   } else if (dev == TRUE) {
     filepath <- paste0(data_folder, pub_day,  "/", out_folder, "/",
                        pub_day, dash, filename, "_dev_version.", type)
   }
-  
+
   if (overwrite == F && file.exists(filepath)) {
     print("The file already exists, please change the parameter overwrite to = TRUE",
           "if you want to overwrite it")
   } else {
-  
+
   # Saving file depending on type
   if (type == "csv") {
     readr::write_csv(dataset, filepath)
   } else if (type == "rds") {
     saveRDS(dataset, filepath)
-    
+
   } else if (type == "xlsx" & out_folder != "tde") {
     openxlsx::saveWorkbook(dataset, filepath, overwrite = TRUE)
-    
+
   } else if (type == "xlsx" & out_folder == "tde") {
-    
-    openxlsx::write.xlsx(dataset, filepath, sheetName = filename)
+
+    # Max sheet name length is 31, so truncate to that
+    openxlsx::write.xlsx(dataset, filepath,
+                         sheetName = substr(filename, 1, 31))
   }
-  
+
   }
 }
 
