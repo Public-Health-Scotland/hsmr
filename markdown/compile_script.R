17e799797738b5345bfe82e3ec38654aeaa71421
peter-menzies-phs <peterm18@nssrstudio06.csa.scot.nhs.uk>
2022-04-07 16:00:12 +0100

    generally working to compile whole report. Still issues with tables

---
+++ markdown/compile_script.R
@@ -0,0 +1,11 @@
+library(phstemplates)
+library(here)
+here()
+
+compile_report(rmd_filename = "markdown/PHS-NATIONAL-STATS-REPORT.Rmd", 
+               cover_filename = "markdown/phs-natstats-cover.docx", 
+               filename_out = "markdown/PHS-NATIONAL-STATS-REPORT-TEST.docx",
+               title = "Hospital Standardised Mortality Ratio",
+               subtitle = "October 2020 to September 2021",
+               date = "08 February 2022")
+
