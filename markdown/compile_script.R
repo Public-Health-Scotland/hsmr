005361e36f22f58aa0f4e8fa5f1298343ce0d2b6
peter-menzies-phs <peterm18@nssrstudio05.csa.scot.nhs.uk>
2022-04-11 10:27:45 +0100

    fix template name in report md and compile script

--- markdown/compile_script.R
+++ markdown/compile_script.R
@@ -4,7 +4,7 @@ here()
 
 compile_report(rmd_filename = "markdown/PHS-NATIONAL-STATS-REPORT.Rmd", 
                cover_filename = "markdown/phs-natstats-cover.docx", 
-               filename_out = "markdown/PHS-NATIONAL-STATS-REPORT-TEST.docx",
+               filename_out = "markdown/PHS-NATIONAL-STATS-REPORT.docx",
                title = "Hospital Standardised Mortality Ratio",
                subtitle = "October 2020 to September 2021",
                date = "08 February 2022")
