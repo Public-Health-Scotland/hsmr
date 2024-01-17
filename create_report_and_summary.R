99e90b5a14c66f88aff37989aa93b8af6d2326a1
paulfe02 <fejiropaul95@gmail.com>
2024-01-17 15:03:28 +0000

    updating markdown templates to PHS template for accredited official statistics

--- create_report_and_summary.R
+++ create_report_and_summary.R
@@ -11,11 +11,11 @@ library(phstemplates)
 
 # creating publication's report
 compile_report(rmd_filename = "markdown/PHS-NATIONAL-STATS-REPORT.Rmd", 
-               cover_filename = "markdown/phs-natstats-cover.docx", 
+               cover_filename = "markdown/phs-accrstats-cover.docx", 
                filename_out = "markdown/PHS-NATIONAL-STATS-REPORT.docx",
                title = "Hospital Standardised Mortality Ratio",
-               subtitle = "July 2022 to June 2023",
-               date = "14 November 2023")
+               subtitle = "October 2022 to September 2023",
+               date = "13 February 2024")
 
 # Creating publication's summary
 rmarkdown::render("markdown/PHS-NATIONAL-STATS-SUMMARY.Rmd")
