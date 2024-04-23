ecde8568605709a1e7382f0f6a70b9ad46ed89eb
charlh07 <charlh07@phs.scot>
2024-04-23 16:31:50 +0100

    Completeness estimate update

--- setup_environment.R
+++ setup_environment.R
@@ -42,8 +42,8 @@ library(xfun)          # For converting numbers to words
 # Note this will not update the documentation.
 # Ctrl+Shift+B > install package > restart R to do that.
 # (Help returns error until R is restarted => cannot do programmatically.)
-# devtools::install(build = FALSE, dependencies = FALSE, upgrade = "never",
-#                   quiet = TRUE)
+devtools::install(build = FALSE, dependencies = FALSE, upgrade = "never",
+                  quiet = TRUE)
 
 library(hsmr)          # For HSMR functions
 
