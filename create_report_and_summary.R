d86dea7d238d1fa7352e255c59bca59b75c121a5
charlh07 <charlh07@phs.scot>
2024-04-25 11:42:33 +0100

    date changes on front page, 3->3.5% in report

--- create_report_and_summary.R
+++ create_report_and_summary.R
@@ -14,8 +14,8 @@ compile_report(rmd_filename = "markdown/PHS-ACCREDITED-STATS-REPORT.Rmd",
                cover_filename = "markdown/phs-accrstats-cover.docx", 
                filename_out = "markdown/PHS-ACCREDITED-STATS-REPORT.docx",
                title = "Hospital Standardised Mortality Ratio",
-               subtitle = "October 2022 to September 2023",
-               date = "13 February 2024")
+               subtitle = "January 2023 to December 2023",
+               date = "14 May 2024")
 
 # Creating publication's summary
 rmarkdown::render("markdown/PHS-ACCREDITED-STATS-SUMMARY.Rmd")
